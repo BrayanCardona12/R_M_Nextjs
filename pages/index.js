b333857297c40301a20ad9e6477381fbefa70c0a
Brayan Cardona <brayancardona3778@gmail.com>
2023-04-13 17:56:30 -0500

    arreglando proyecto

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import Container from "../components/Container"
 import CardUsers from "../components/Users"
@@ -24,31 +23,4 @@ Index.getInitialProps = async (ctx) => {
     return { data }
 }
 
-=======
-
-import Container from "../components/Container"
-import CardUsers from "../components/Users"
-
-const Index = (datos) => {
-
-    return (
-        <>
-            <Container>
-                <h1 className="text-center">Characters</h1>
-                <div className="mw-100 min-vh-100 d-flex flex-wrap justify-content-center">
-                    <CardUsers user={datos} />
-                </div>
-            </Container>
-        </>
-    )
-}
-
-Index.getInitialProps = async (ctx) => {
-    const resp = await fetch('https://rickandmortyapi.com/api/character')
-    const data = await resp.json()
-
-    return { data }
-}
-
->>>>>>> 0a1fa938f31853915a1fd118dccba0be2d8a4611
 export default Index
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useState } from 'react'
 import Container from '../../components/Container'
 
@@ -31,36 +30,4 @@ UserProfile.getInitialProps = async (ctx) => {
 
 }
 
-=======
-import {useRouter} from 'next/router'
-import Container from '../../components/Container'
-
-
-const UserProfile = (props) => {
-    console.log(props)
-
-
-    
-    return(
-        <Container>
-            <div style={{backgroundColor: "#C4C5D0"}} className='rounded d-flex flex-column align-items-center'>
-                <img className='img-fluid img-thumbnail m-3' src={props.data.image} alt='photo-character'/>
-                <h1>{props.data.name}</h1>  
-                <p>{props.data.origin.name} | {props.data.status == 'Alive'? "🟢" : "🔴"} {props.data.status} </p>
-            </div>
-        </Container>
-   
-    )
-}
-
-UserProfile.getInitialProps = async (ctx) => {
-    
-    const resp = await fetch(`https://rickandmortyapi.com/api/character/${ctx.query.id}`)
-    const data = await resp.json()
-    
-    return {data: data}
-
-}
-
->>>>>>> 0a1fa938f31853915a1fd118dccba0be2d8a4611
 export default UserProfile
