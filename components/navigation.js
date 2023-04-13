140f607b74c8bcd6e17b46fa111f732e6622e64d
Brayan <114372854+BrayanCardona12@users.noreply.github.com>
2023-04-13 16:40:04 -0500

    modificando estilos

--- components/navigation.js
+++ components/navigation.js
@@ -1,40 +1,40 @@
 import Link from 'next/link'
 
 const Navigation = () => {
-    return(
-        <nav className="navbar bg-primary navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <Link href='/' className='navbar-brand'>Project | Next.js</Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className='nav-link' href='/'>Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/about">About</Link>
-                
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" href="/services">Services</Link>
-                 
-              </li>
-            </ul>
-          </div>
+  return (
+    <nav className="navbar bg-primary navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link href='/' className='navbar-brand'>Project | Next.js</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="bi bi-list success text-success" style={{fontSize: "1.5em"}}></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className='nav-link' href='/'>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/about">About</Link>
+
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/services">Services</Link>
+
+            </li>
+          </ul>
         </div>
-      </nav>
-      
-    )
+      </div>
+    </nav>
+
+  )
 }
 
 export default Navigation
--- pages/about.js
+++ pages/about.js
@@ -9,11 +9,12 @@ const About = () => {
                 <title>Home | Next.js</title>
             </Head>
             <Container>
-                <h1>About</h1>
-                <p>
-                orem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-                </p>
+                <h1 className='text-center'>About</h1>
+                <p className="text-justify">
+                Lorem ipsum dolor sit amet. Et culpa deserunt sit aspernatur deserunt et quisquam voluptate aut enim aliquam id consectetur sequi eum nisi maxime. A eveniet culpa et ipsam nesciunt ut consequuntur voluptatem et perferendis natus ea molestias quisquam! Est maiores quas in sunt fugit et inventore eius ut voluptatibus porro et ipsum accusamus.
+                A similique sapiente non voluptatem sapiente qui necessitatibus culpa eum nihil tempora aut doloremque architecto. Qui galisum voluptate est pariatur quia vel tempora eius.
+                At debitis soluta et odio blanditiis id ipsa eligendi ut repellat facilis qui perspiciatis modi aut mollitia corrupti eos dolores illum. Vel praesentium quisquam et nostrum omnis in quia architecto et dolorem tempora 33 accusamus molestias ut totam quia et nihil corporis! Sed dolorem quaerat est aperiam beatae eos dolorem magni et perspiciatis autem qui reprehenderit delectus.
+            </p>
             </Container>
         </>
 
